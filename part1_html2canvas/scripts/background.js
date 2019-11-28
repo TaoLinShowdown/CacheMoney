efd3a3a2750d2111e705bc23fe6b0b56dde21b19
Tao Lin <47548247+LinTao5711y@users.noreply.github.com>
2019-11-28 17:29:22 -0500

    made overlay and dialog that closes the overlay

--- part1_html2canvas/scripts/background.js
+++ part1_html2canvas/scripts/background.js
@@ -1,5 +1,5 @@
 var tabDataStore = {};
-var interval = 5000;
+var interval = 2000;
 
 // THE MAIN PART WHERE WE TAKE A SNAPSHOT EVERY COUPLE SECONDS AND STORE IT LOCALLY
 var t = setInterval(function() {
@@ -17,7 +17,8 @@ var t = setInterval(function() {
                 }
                 
                 // TAKE A SCREENSHOT AND STORE IT IN THE CORRECT TAB'S ENTRY
-                chrome.tabs.captureVisibleTab(null, {}, function(dataURL) {
+                chrome.tabs.captureVisibleTab(null, {format: "png"}, function(dataURL) {
+                    // SCROLL BAR IS 17px
                     tabDataStore['tab_' + id].img = dataURL;
                     console.log("snapshot of tab_" + id);
                     console.table(tabDataStore);
@@ -34,12 +35,18 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
     // CHECK IF THERE IS ALREADY A SCREENSHOT FOR THAT TAB, IF NOT DO NOTHING
     if(tabDataStore['tab_' + tabId] !== undefined) {
         if(tabDataStore['tab_' + tabId].img !== undefined) {
-            chrome.tabs.captureVisibleTab(null, {}, function(dataURL) {
+            chrome.tabs.captureVisibleTab(null, {format: "png"}, function(dataURL) {
                 // THIS IS WHERE RESEMBLE COMES IN AND COMPARES THE OLD SCREENSHOT WITH THIS NEW ONE
 
 
                 // TAKE THE ARRAY OF COORDS WHERE WE DETECT CHANGE AND SEND A MESSAGE TO CONTENT SCRIPT TO BUILD AND APPLY OVERLAY TO PAGE
-
+                console.log("sending message to content script from " + tabId);
+                var testcoords = [ [5, 3], [5, 4], [5, 5],
+                                    [6, 3], [6, 4], [6, 5],
+                                    [7, 3], [7, 4], [7, 5] ];
+                chrome.tabs.sendMessage(tabId, {coords: testcoords}, function(response) {
+                    console.log("Content script response: ", response.handshake);
+                });
 
             })
         }
--- part1_html2canvas/scripts/screenshot.js
+++ part1_html2canvas/scripts/screenshot.js
@@ -1,26 +1,24 @@
-console.log(document.body);
+// MAKING THE OVERLAY THAT WILL HIGHLIGHT PARTS OF THE WEBPAGE THAT HAVE CHANGED
+var changes = [] // ARRAY OF COORDS WHERE CHANGES WERE DETECTED
 
 var makeOverlay = function() {
-    var h = document.body.clientHeight;
-    var w = document.body.clientWidth;
     var overlay = document.createElement("div");
     overlay.id = "tabnab-overlay"
-    overlay.style.position = "absolute";
+    overlay.style.position = "fixed";
     overlay.style.top = 0;
     overlay.style.left = 0;
     overlay.style.right = 0;
     overlay.style.bottom = 0;
-    overlay.style.zIndex = 99999;
-    // overlay.style.height = h + "px";
-    // overlay.style.width = w + "px";
+    overlay.style.zIndex = 99998;
     overlay.style.height = "100%";
     overlay.style.width = "100%";
-    for(var i = 0; i < h/10 + 1; i++) {
+    overlay.style.background = "rgba(0, 0, 0, 0.3)";
+    for(var i = 0; i < window.innerHeight/10; i++) {
         var row = document.createElement("div");
         row.style.height = 10 + "px";
         row.style.display = "flex";
         row.style.flexDirection = "wrap";
-        for(var j = 0; j < w/10 + 1; j++) {
+        for(var j = 0; j < (window.innerWidth - 17)/10 + 1; j++) {
             var box = document.createElement("div");
             box.id = i + " " + j;
             box.style.position = "relative";
@@ -31,12 +29,114 @@ var makeOverlay = function() {
         }
         overlay.appendChild(row);
     }
-    // document.body.appendChild(overlay);
+    return overlay;
 }
 
-// chrome.runtime.sendMessage({msg: "capture"}, function(response) {
-//     // console.log(response.imgSrc);
-//     var img = new Image;
-//     img.src = response.imgSrc;
-//     document.body.appendChild(img);
-// });
+var makeDialog = function() {
+    var dialog = document.createElement("div");
+    dialog.id = "tabnab-dialog"
+    dialog.style.position = "fixed";
+    dialog.style.right = 0;
+    dialog.style.bottom = 0;
+    dialog.style.background = "rgba(255, 255, 255, 0.7)";
+    dialog.style.width = "200px";
+    dialog.style.height = "100px";
+    dialog.style.zIndex = 99999;
+    dialog.style.display = "flex";
+    dialog.style.flexDirection = "column";
+    dialog.style.alignItems = "center";
+    dialog.style.justifyContent = "center";
+    dialog.style.borderTopLeftRadius = "3px";
+    dialog.style.borderTopRightRadius = "3px";
+    dialog.style.fontFamily = "Calibri";
+    dialog.style.fontSize = "11px";
+    dialog.style.textAlign = "center";
+    dialog.innerHTML = "CacheMoney detected changes in <br> this tab which are highlighed in red <br> Would you like to report this website?";
+
+    var buttonsContainer = document.createElement("div");
+    var closeButton = document.createElement("button");
+    closeButton.innerHTML = "Close"
+    closeButton.onclick = () => {
+        enableScroll();
+        document.body.removeChild(document.getElementById('tabnab-overlay'));
+        document.body.removeChild(document.getElementById('tabnab-dialog'));
+    }
+    var submitButton = document.createElement("button");
+    submitButton.innerHTML = "Report"
+    submitButton.onclick = () => {
+        enableScroll();
+        document.body.removeChild(document.getElementById('tabnab-overlay'));
+        document.body.removeChild(document.getElementById('tabnab-dialog'));
+        console.log("URL of tab is: lol idk")
+    }
+    buttonsContainer.style.padding = "3px";
+    buttonsContainer.appendChild(closeButton);
+    buttonsContainer.appendChild(submitButton);
+
+    dialog.appendChild(buttonsContainer);
+
+    return dialog;
+}
+
+console.log("content script loaded");
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    sendResponse({handshake: "Coords received"});
+    
+    disableScroll();
+    
+    var { coords } = request;
+    changes = [];
+    changes = [ ...coords ];
+    console.log("changes that were detected: ", changes);
+
+    var overlay = makeOverlay();
+    var dialog = makeDialog();
+    document.body.appendChild(overlay);
+    document.body.appendChild(dialog);
+
+    // COLORING IN THE BOXES THAT WE DETECTED CHANGE IN
+    changes.forEach((coord) => {
+        var id = coord[0] + " " + coord[1];
+        document.getElementById(id).style.background = "rgba(224, 0, 0, 0.37)";
+    })
+});
+
+// CODE FROM: https://stackoverflow.com/questions/4770025/how-to-disable-scrolling-temporarily
+//
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault)
+        e.preventDefault();
+    e.returnValue = false;  
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+    if (window.addEventListener) // older FF
+        window.addEventListener('DOMMouseScroll', preventDefault, false);
+    document.addEventListener('wheel', preventDefault, {passive: false}); // Disable scrolling in Chrome
+    window.onwheel = preventDefault; // modern standard
+    window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+    window.ontouchmove  = preventDefault; // mobile
+    document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    document.removeEventListener('wheel', preventDefault, {passive: false}); // Enable scrolling in Chrome
+    window.onmousewheel = document.onmousewheel = null; 
+    window.onwheel = null; 
+    window.ontouchmove = null;  
+    document.onkeydown = null;  
+}
